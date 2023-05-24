fb2235cd4dd43b3546c6b8cbeebdbcb3ef2eb13e
Roasteg <krostislav468@gmail.com>
2023-05-24 13:42:25 +0300

    Move page switching into separate function

--- commands/search/search.command.ts
+++ commands/search/search.command.ts
@@ -1,9 +1,10 @@
-import { Composer } from "telegraf";
+import { Composer, Context, NarrowedContext } from "telegraf";
 import editMessage from "../../helper/editMessage";
 import getSongsString from "../../helper/getSongsString";
 import searchSongs from "./searchSongs";
 import bot from "../../config/bot";
 import SearchService from "../../service/SearchService";
+import { CallbackQuery, Update } from "telegraf/typings/core/types/typegram";
 
 const searchService = new SearchService();
 
@@ -50,8 +51,14 @@ module.exports = Composer.command("search", async (context) => {
     }
 });
 
-bot.action("next", async (context) => {
-    searchService.page += 1;
+const switchPage = async (
+    context: NarrowedContext<
+        Context<Update> & {
+            match: RegExpExecArray;
+        },
+        Update.CallbackQueryUpdate<CallbackQuery>
+    >
+) => {
     try {
         const songs = await searchSongs(
             searchService.searchString,
@@ -69,26 +76,14 @@ bot.action("next", async (context) => {
     } catch (error) {
         context.reply(String(error));
     }
+};
+bot.action("next", async (context) => {
+    searchService.page += 1;
+    await switchPage(context);
 });
 
 bot.action("previous", async (context) => {
     if (searchService.page === 0) return context.answerCbQuery();
     searchService.page -= 1;
-    try {
-        const songs = await searchSongs(
-            searchService.searchString,
-            searchService.page * 5
-        );
-        if (songs.length > 0) {
-            await context.editMessageText(`Found!\n${getSongsString(songs)}`, {
-                reply_markup: responseKeyboard,
-            });
-        } else {
-            await context.editMessageText("No more tabs", {
-                reply_markup: responseKeyboard,
-            });
-        }
-    } catch (error) {
-        context.reply(String(error));
-    }
+    await switchPage(context);
 });
